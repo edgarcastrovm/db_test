3b36a92b541948c727fca4185daf0e8254d69969
Edgar Castro <edgar.castro.vm@gmail.com>
2022-12-06 10:28:02 -0500

    Update db.sh
    add parameters

--- db.sh
+++ db.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-ORACLE_SID=xe; export ORACLE_SID
-SCRIPT=/c/Scripts; export SCRIPT
-USER_ORA='ECASTRO'; export USER_ORA
-PASS_ORA='Novopayment1*'; export PASS_ORA
-SERVER_ORA='192.168.100.200'; export SERVER_ORA
-SERVER_PORT=1522; export SERVER_PORT
-echo "$USER_ORA/$PASS_ORA@$SERVER_ORA:$SERVER_PORT/$ORACLE_SID"
-#docker run  -ti --rm ghcr.io/oracle/oraclelinux7-instantclient:19 
-#sqlplus $USER_ORA/$PASS_ORA@$SERVER_ORA:$SERVER_PORT/$ORACLE_SID @$SCRIPT/genera_reportes.sql DEMO_PRODUCT_INFO
+#Credenciales oracle
+ORACLE_SID=xe
+SCRIPT=db.sql
+USER_ORA='ECASTRO'
+PASS_ORA='Novopayment1*'
+SERVER_ORA='192.168.100.200'
+SERVER_PORT=1522
+##------------------------------------------------------------------------------------------------
+##---- Run command from string
+#TABLE=$1
+#cmd="sqlplus $USER_ORA/$PASS_ORA@$SERVER_ORA:$SERVER_PORT/$ORACLE_SID @$SCRIPT $TABLE"
+#echo $cmd
+#eval $cmd
 
-
-
-docker run  -ti --rm ghcr.io/oracle/oraclelinux7-instantclient:19 sqlplus ECASTRO/Novopayment1*@localhost:1522/xe
+##------------------------------------------------------------------------------------------------
+##---- Run command with parameters script
+sqlplus $USER_ORA/$PASS_ORA@$SERVER_ORA:$SERVER_PORT/$ORACLE_SID @$SCRIPT $1
